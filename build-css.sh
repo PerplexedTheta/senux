688abe5e2e177c5f02da1342482fd52f605ef5b0
Jake Deery <jake.deery@ptfs-europe.com>
2022-03-21 11:24:39 +0000

    Complete rewrite of scripts

--- build-css.sh
+++ build-css.sh
@@ -1,29 +1,36 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#
-# a script to build the scss files and output them as dist/Senux Project.css
-#
-# required: npm
-# deb:		sudo apt install npm -y
-# brew:		brew install npm
-# winget:	winget install -e --id OpenJS.Nodejs
-#
-# required: sass
-# npm:		sudo npm install -g sass
-#
-
 if [[ ! $(command -v sass) ]]; then
 	echo 'You need to install sass first. Please install npm (sudo apt install npm -y) then fetch sass (sudo npm install -g sass)'
 	exit 1
 fi
 
-if [[ $1 == "--sample-run" ]]; then
-	sass ${SCRIPT_DIR}/src/css/build.sample.scss:${SCRIPT_DIR}/dist/OPACUserCSS.sample.css
-	echo "File built and outputted to ./dist/OPACUserCSS.sample.css"
-else
+if [[ $1 == "--init" ]]; then
+	cp ${SCRIPT_DIR}/dist/variables.sample.scss ${SCRIPT_DIR}/variables.scss
+	cp ${SCRIPT_DIR}/dist/customisations.sample.scss ${SCRIPT_DIR}/customisations.scss
+	echo "Copied sample files to "${SCRIPT_DIR}
+	echo "Now, run "${SCRIPT_DIR}"/build-css.sh --build"
+elif [[ $1 == "--build" ]]; then
 	sass ${SCRIPT_DIR}/src/css/build.scss:${SCRIPT_DIR}/dist/OPACUserCSS.css
 	echo "File built and outputted to ./dist/OPACUserCSS.css"
+elif [[ $1 == "--minify" ]]; then
 	sass ${SCRIPT_DIR}/src/css/build.scss:${SCRIPT_DIR}/dist/OPACUserCSS.min.css --style compressed
 	echo "File built and outputted to ./dist/OPACUserCSS.min.css"
+else
+	echo "a script to build the scss files and output them as dist/Senux Project.css"
+	echo ""
+	echo "required: npm"
+	echo "deb:		sudo apt install npm -y"
+	echo "brew:		brew install npm"
+	echo "winget:		winget install -e --id OpenJS.Nodejs"
+	echo ""
+	echo "required: sass"
+	echo "npm:		sudo npm install -g sass"
+	echo ""
+	echo ""
+	echo "available arguments:"
+	echo "--init		run with this flag to prep this repository clone for a build"
+	echo "--build		run with this flag to build the css to 'dist'"
+	echo "--minify	run with this flag to build the minified css to 'dist'"
 fi
--- build-js.sh
+++ build-js.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#
-# a script to build the scss files and output them as dist/Senux Project.css
-#
-# required: npm
-# deb:		sudo apt install npm -y
-# brew:		brew install npm
-# winget:	winget install -e --id OpenJS.Nodejs
-#
-# required: minify
-# npm:		sudo npm install -g minify
-#
-
 if [[ ! $(command -v minify) ]]; then
 	echo 'You need to install minify first. Please install npm (sudo apt install npm -y) then fetch minify (sudo npm install -g minify)'
 	exit 1
 fi
 
-if [[ $1 == "--sample-run" ]]; then
-	# we do nothing here
-	echo -n ''
-else
+if [[ $1 == "--init" ]]; then
+	cp ${SCRIPT_DIR}/dist/customisations.sample.js ${SCRIPT_DIR}/customisations.js
+	echo "Copied sample files to "${SCRIPT_DIR}
+	echo "Now, run "${SCRIPT_DIR}"/build-js.sh --build"
+elif [[ $1 == "--build" ]]; then
+	cp ${SCRIPT_DIR}/customisations.js ${SCRIPT_DIR}/dist/OPACUserJS.js
+	echo "File built and outputted to ./dist/OPACUserJS.js"
+elif [[ $1 == "--minify" ]]; then
 	minify ${SCRIPT_DIR}/dist/OPACUserJS.js > ${SCRIPT_DIR}/dist/OPACUserJS.min.js
+	echo "File built and outputted to ./dist/OPACUserJS.min.js"
+else
+	echo "a script to build the js files and output them as dist/Senux Project.js"
+	echo ""
+	echo "required: npm"
+	echo "deb:		sudo apt install npm -y"
+	echo "brew:		brew install npm"
+	echo "winget:		winget install -e --id OpenJS.Nodejs"
+	echo ""
+	echo "required: minify"
+	echo "npm:		sudo npm install -g minify"
+	echo ""
+	echo ""
+	echo "available arguments:"
+	echo "--init		run with this flag to prep this repository clone for a build"
+	echo "--build		run with this flag to build the js to 'dist'"
+	echo "--minify	run with this flag to build the minified js to 'dist'"
 fi
-echo "File built and outputted to ./dist folder"
