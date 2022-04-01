5a313f1f7ed8acd868319b4114853bc2fd8da6b0
Jake Deery <jake.deery@ptfs-europe.com>
2022-04-01 13:51:18 +0000

    Added licence to output

--- dist/customisations.sample.js
+++ dist/customisations.sample.js
@@ -1,3 +1,31 @@
+/*!
+  Copyright 2021-2022 Jake Deery & PTFS Europe
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+/*!
+  WELCOME TO SENUX! VER. 2111-04 -- HAVE A NICE DAY!
+
+  USER ALERT: DO NOT MODIFY ANY OF THE CONTENT HELD WITHIN THE OPACUSERJS
+  SYSPREF. INSTEAD, PLEASE MAKE ALL JS CHANGES WITHIN THE CUSTOMISATIONS.JS
+  FILE HOUSED WITHIN THE SENUX GIT REPOSITORY. THEN, RUN THE BUILD SCRIPT(S)
+  AND FETCH THE OUTPUT FROM THE DIST DIRECTORY.
+
+  THIS RESTRICTION IS IN PLACE BECAUSE A SCRIPT IS USED TO GENERATE THE JS.
+  MODIFYING THE JS OUTSIDE OF RUNNING THIS SCRIPT CAN CAUSE ISSUES, AND LOSS
+  IN CUSTOMISATIONS FURTHER DOWN THE LINE. YOU HAVE BEEN WARNED!
+*/
+
 // OPACUserJS, Senux Project, Jake Deery, 2021
 document.addEventListener('DOMContentLoaded', function(event) {
 	// vars
